75901165a1224d5ba322a1286b5c01ebde32b149
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2023-05-14 18:30:14 -0400

    5.2

--- main.cc
+++ main.cc
@@ -3,8 +3,29 @@
 #include "ray.h"
 #include "color.h"
 
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    // t^2 * (b⋅b) + 2t * (b⋅(𝐀−𝐂)) + (𝐀−𝐂)⋅(𝐀−𝐂) − r^2 = 0
+    // ^^^^^^^^^^^       ^^^^^^^^^^    ^^^^^^^^^^^^^^^^^
+    //    A                   B                C
+    // a: origin, b: direction
+
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * (dot(r.direction(), oc));
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminant = b * b - 4 * a * c;
+
+    return discriminant > 0;
+}
+
 color ray_color(const ray &r)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return ((1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0));
