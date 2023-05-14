e348d45b1ad8e4d3ab0a3c36e60b3dde93467df0
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2023-05-14 18:15:05 -0400

    finished 4.2

--- main.cc
+++ main.cc
@@ -1,21 +1,45 @@
 #include <iostream>
 #include "vec3.h"
+#include "ray.h"
 #include "color.h"
 
-int main() {
-    // Image
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return ((1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0));
+}
 
+int main()
+{
+    // Image
+    const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 256;
-    const int image_height = 256;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
-    std::cout<< "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height-1; j>=0; --j) {
-        std::cerr << "\rScanlines Remaining: " << j << ' '<< std::flush;
-        for (int i = 0; i < image_width; i++) {
-            color pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
-            write_color(std::cout, pixel_color);
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines Remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            write_color(std::cout, ray_color(r));
         }
     }
     std::cerr << "\nDone\n";
